f8896be9736bc5903bd60698affb766aa397c791
gaochong <mercury1259@outlook.com>
2017-07-26 15:12:50 +0800

    创建库

---
+++ src/codes/CoreXLibrary.Email/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreXLibrary.Email
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/codes/CoreXLibrary.SqlHelper/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreXLibrary.SqlHelperDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
